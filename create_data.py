e961a45c34c3468f6f19bc9b808bf839005e472b
Felix Weidemann <felix>
2025-10-31 10:56:03 +0100

    the data became more realistic

--- create_data.py
+++ create_data.py
@@ -1,8 +1,7 @@
-from datetime import datetime
+from datetime import datetime, date
 import time
-from random import randint, choice
+from random import randint, choice, shuffle
 from faker import Faker
-import pandas as pd
 from pathlib import Path
 from rich import print as rprint
 import csv
@@ -12,9 +11,9 @@ import asyncio
 
 f = Faker()
 
-def create_random_datetimes() -> datetime:
+def create_random_datetimes(start_date: date) -> datetime:
     """Creates a random datetime"""
-    return f.date_time_between()
+    return f.date_time_between(start_date)
 
 def create_customer() -> dict:
     """Creates a synthetic Customer"""
@@ -22,41 +21,50 @@ def create_customer() -> dict:
     user['customer_id'] = "".join([str(randint(1,9)) for x in range(10)])
     user['first_name'] = f.first_name()
     user['last_name'] = f.last_name()
+    user['Gender'] = choice(["F", "M"])
+    user['Birthday'] = f.date_between(date(1900, 1, 1), date(2000, 12, 31))
     user['city'] = f.city()
     return user
 
 def create_services() -> list:
     """Returns a choice of a Service"""
     services = [
-        {"service_name": "Databases", "costs_per_month": 9.99}, 
-        {"service_name": "E-Commerce", "costs_per_month": 39.99}, 
-        {"service_name": "Streaming", "costs_per_month": 19.99}, 
-        {"service_name": "Automation", "costs_per_month": 89.99}, 
-        {"service_name": "Bi Tooling", "costs_per_month": 8.99}, 
-        {"service_name": "Hardware", "costs_per_month": 12.69},
-        {"service_name": "Custom Development", "costs_per_month": 199.99}
+        {"service_name": "Databases", "costs": 9.99}, 
+        {"service_name": "E-Commerce", "costs": 39.99}, 
+        {"service_name": "Streaming", "costs": 19.99}, 
+        {"service_name": "Automation", "costs": 89.99}, 
+        {"service_name": "Bi Tooling", "costs": 8.99}, 
+        {"service_name": "Hardware", "costs": 12.69},
+        {"service_name": "Custom Development", "costs": 199.99}
     ]
-    return choice(services)
+    return services
 
 def create_status() -> str:
     return choice(["Success", "Failure"])
     
-async def make_single_entry() -> tuple:
+async def make_single_entry(customer: dict) -> tuple:
     """Creates Random Data. Because of the keyword
     async, returns a coroutine"""    
-    usage_times = create_random_datetimes()
-    customers = create_customer()
-    services = create_services()
+    usage_times = create_random_datetimes(date(2010, 1, 1))
+    services = choice(create_services())
     status = create_status()
+    
     return (
         usage_times,
-        customers,
+        customer,
         services,
         status)
 
-async def generate_data_async(number: int) -> list:
+async def generate_data_async(max_services: int, max_customers: int) -> list:
     """Generates data asynchronously. Returns a list with coroutines"""
-    tasks = [make_single_entry() for _ in range(number)]
+    customers = [create_customer() for _ in range(max_customers)]
+    tasks = [
+        make_single_entry(customer)
+        for customer in customers
+        for _ in range(max_services)
+    ]
+    # shuffle makes data even more realistic
+    shuffle(tasks)
     return await asyncio.gather(*tasks)
 
 def export_as_csv(data: list, target: Path) -> None:
@@ -65,9 +73,11 @@ def export_as_csv(data: list, target: Path) -> None:
         "Customer ID",
         "First_Name",
         "Last_Name",
+        "Gender",
+        "Birthday",
         "City",
         "Servicename",
-        "Costs per Month",
+        "Costs",
         "Status last use"]
     
     if len(data) == 0:
@@ -85,9 +95,11 @@ def export_as_csv(data: list, target: Path) -> None:
                         item[1]['customer_id'],
                         item[1]['first_name'],
                         item[1]['last_name'],
+                        item[1]['Gender'],
+                        item[1]['Birthday'],
                         item[1]['city'],
                         item[2]['service_name'],
-                        item[2]['costs_per_month'],
+                        item[2]['costs'],
                         item[3]
                     ]
                 )
@@ -102,14 +114,13 @@ if __name__ == "__main__":
 
     start = time.time()
     # execute the coroutines
-    generated_data = asyncio.run(generate_data_async(1)) 
-    rprint(generated_data)
-    ende = time.time()
-    rprint(f"The execution was completed in {(ende - start):.2f} seconds!")
-    rprint("[bold green]Done![/bold green]")
+    # creates max_customers * max_services entrys
+    generated_data = asyncio.run(generate_data_async(max_customers=100, max_services=10)) 
 
     rprint("[bold blue]Saving to csv...[/bold blue]")
     export_as_csv(generated_data, current_path / "data" / "example.csv")
-    # df = convertto_dataframe(generated_data)
-    # df.to_csv(current_path / "data" / "data.csv", index=False)
     rprint("[bold blue]Done![/bold blue]")
+
+    ende = time.time()
+    rprint(f"The execution was completed in {(ende - start):.2f} seconds!")
+    rprint("[bold green]Done![/bold green]")
