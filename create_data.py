2ae51a12daf6cd8d511acd053798899bde9d83b8
Felix Weidemann <felix>
2025-10-31 09:02:49 +0100

    add export_as_csv

--- create_data.py
+++ create_data.py
@@ -5,6 +5,7 @@ from faker import Faker
 import pandas as pd
 from pathlib import Path
 from rich import print as rprint
+import csv
 
 import asyncio
 
@@ -74,6 +75,35 @@ def convertto_dataframe(data) -> pd.DataFrame:
         result.append(tmp)
     return pd.DataFrame(result)
 
+def export_as_csv(data: str, target: Path) -> None:
+    header = [
+        "Usage Time",
+        "Customer ID",
+        "First_Name",
+        "Last_Name,City",
+        "Servicename",
+        "Costs per Month",
+        "Status last use"]
+    with open(target, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=";", quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(header)
+        if len(data) == 0:
+            rprint("Die Daten entsprechen nicht den Vorgaben!")
+            return
+        for item in data:
+            writer.writerow(
+                [
+                    item[0],
+                    item[1]['customer_id'],
+                    item[1]['first_name'],
+                    item[1]['last_name'],
+                    item[1]['city'],
+                    item[2]['service_name'],
+                    item[2]['costs_per_month'],
+                    item[3]
+                ]
+            )
+
 
 if __name__ == "__main__":
     current_path = Path(__file__).parent
@@ -82,12 +112,14 @@ if __name__ == "__main__":
 
     start = time.time()
     # execute the coroutines
-    generated_data = asyncio.run(generate_data_async(100)) 
+    generated_data = asyncio.run(generate_data_async(1)) 
+    rprint(generated_data)
     ende = time.time()
     rprint(f"The execution was completed in {(ende - start):.2f} seconds!")
     rprint("[bold green]Done![/bold green]")
 
     rprint("[bold blue]Saving to csv...[/bold blue]")
-    df = convertto_dataframe(generated_data)
-    df.to_csv(current_path / "data.csv", index=False)
+    export_as_csv(generated_data, current_path / "data" / "example.csv")
+    # df = convertto_dataframe(generated_data)
+    # df.to_csv(current_path / "data" / "data.csv", index=False)
     rprint("[bold blue]Done![/bold blue]")
